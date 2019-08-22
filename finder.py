7e057aa1849ac56208cfa962f15f7cc3ed7b9134
Kashyap Achar <31697626+kashrocks@users.noreply.github.com>
2019-08-22 11:33:24 -0400

    added in additional functionalty

--- finder.py
+++ finder.py
@@ -2,6 +2,7 @@ import xlrd
 import requests
 from bs4 import BeautifulSoup
 import os
+import pandas as pd
 
 cwd = os.getcwd()
 in_lever = 0
@@ -32,3 +33,51 @@ for i in range(1, 46):
 
 print('')
 print('Total companies in lever: ' + str(in_lever))
+
+
+# -----------------------------------
+# hits all companies and checks whether a lever plat form exists or not
+
+# def good_comp(comp):
+#     with open('good_comps.txt', 'a') as f:
+#         f.write("%s\n" % comp)
+
+# def bad_comp(comp):
+#     with open('bad_comps.txt', 'a') as f:
+#         f.write("\n" + comp)
+
+# cols = ['name','domain','year founded','industry','size range','locality','country','linkedin url','current employee estimate','total employee estimate']
+# data = pd.read_csv('free_pdl_company_dataset.csv', names=cols)
+# names = data.name.tolist()
+# print("")
+# print('Finished retrieving names, finding one word companies:')
+# print('')
+
+# eligible = []
+# for comp in names:
+#     if isinstance(comp, str): 
+#         if ' ' not in comp:
+#             eligible.append(comp)
+
+# print("")
+# print('Finished retrieving one word names, starting tests:')
+# print('')
+
+# total = len(eligible)
+# use_lever = []
+# no_lever = []
+# i = 0
+# for comp in eligible:
+#     i += 1
+
+#     url = 'https://jobs.lever.co/' + comp
+#     response = requests.get(url)
+#     if response.status_code != 200:
+#         print("Tested " + comp + " was not good. " + str(i) + '/ ' + str(total))
+#         bad_comp(comp)
+#         print(response.status_code)
+#         continue
+#     good_comp(comp)
+#     print(" ")
+#     print("Tested " + comp + " was GOOD. " + str(i) + '/ ' + str(total))
+#     print(" ")
